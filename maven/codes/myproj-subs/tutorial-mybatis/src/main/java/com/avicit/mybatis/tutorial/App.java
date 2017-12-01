3a2191cf8da47389d53f1ad0855784786817ad07
guofeng <guofengzh@gmail.com>
2017-12-01 16:25:24 +0800

    init

---
+++ maven/codes/myproj-subs/tutorial-mybatis/src/main/java/com/avicit/mybatis/tutorial/App.java
@@ -0,0 +1,13 @@
+package com.avicit.mybatis.tutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
